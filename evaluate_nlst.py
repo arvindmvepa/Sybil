4bb424443062f17a86390a57e9cc0c788f340452
Arvind Murari Vepa <arvind.m.vepa@gmail.com>
2026-01-13 08:34:24 -0800

    test eval

--- evaluate_nlst.py
+++ evaluate_nlst.py
@@ -2,6 +2,7 @@ from sybil import Serie, Sybil
 import pandas as pd
 from glob import glob
 import os
+import pyreadstat
 
 
 # Load a trained model
@@ -14,20 +15,29 @@ test_pids = test_df['PID'].tolist()
 
 
 root_img_dir = "/mii/data/lung/nlst/NLST_CT_raw/data"
+patient_file = "participant_d100814.sas7bdat"
+(patient_df, _) = pyreadstat.read_sas7bdat(patient_file)
+
 print(f"Evaluating {len(test_pids)} test patients.")
 for test_pid in test_pids:
+    pid_ann_df = patient_df.loc[patient_df["pid"] == test_pid]
+    cancyr = pid_ann_df['cancyr'].iloc[0]
+    has_cancer = 0
+    if not pd.isna(cancyr):
+        has_cancer = 1
     patient_dir = os.path.join(root_img_dir, str(test_pid))
-    time_points = sorted(glob(os.path.join(patient_dir, "*")))
-    for time_index, time_point in enumerate(time_points):
-        img_dirs = sorted(glob(os.path.join(time_point, "*")))
-        max_img_files = None
-        for img_dir_pos, img_dir in enumerate(img_dirs):
-            img_files = glob(os.path.join(img_dir, "*"))
-            if max_img_files is None or len(img_files) > len(max_img_files):
-                max_img_files = img_files
-                max_img_dir_pos = img_dir_pos
-        # Create a Serie object for the patient
-        serie = Serie(max_img_files)
-        # Get risk score
-        score = model.predict([serie])
-        print(f"Patient ID: {test_pid}, Time Point: {time_index}, Risk Score: {score}")
+    if os.path.exists(patient_dir):
+        time_points = sorted(glob(os.path.join(patient_dir, "*")))
+        for time_index, time_point in enumerate(time_points):
+            img_dirs = sorted(glob(os.path.join(time_point, "*")))
+            max_img_files = None
+            for img_dir_pos, img_dir in enumerate(img_dirs):
+                img_files = glob(os.path.join(img_dir, "*"))
+                if max_img_files is None or len(img_files) > len(max_img_files):
+                    max_img_files = img_files
+                    max_img_dir_pos = img_dir_pos
+            # Create a Serie object for the patient
+            serie = Serie(max_img_files)
+            # Get risk score
+            score = model.evaluate([serie], label=has_cancer)
+            print(f"Patient ID: {test_pid}, Time Point: {time_index}, Risk Score: {score}")
